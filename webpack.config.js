5d142d56cb3ef87a8049bb29f5c99ad0e8bdeb82
OKUMURA Yoshifumi <y-okumura@picosystem.co.jp>
2023-03-14 16:39:02 +0900

    Add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  target: "node",
+  entry: {
+    FuncAlertToTeams: path.resolve(__dirname, "FuncAlertToTeams/index.ts"),
+  },
+  output: {
+    filename: "[name]/index.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "commonjs",
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"],
+    // Add support for TypeScripts fully qualified ESM imports.
+    extensionAlias: {
+      ".js": [".js", ".ts"],
+      ".cjs": [".cjs", ".cts"],
+      ".mjs": [".mjs", ".mts"],
+    },
+  },
+  module: {
+    rules: [
+      // all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+};
